db729e254e0d389704a37fecca86b97d38d826c9
Dhruvdudhat310 <106237977+Dhruvdudhat310@users.noreply.github.com>
2023-03-15 23:33:42 +0530

    Update practical-04.java

--- java programs/practical-04.java
+++ java programs/practical-04.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class prac_4 {
+public class p_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number...");
