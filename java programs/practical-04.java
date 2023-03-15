256e9751f9899422a64b17f02106808f6390068c
Dhruvdudhat310 <106237977+Dhruvdudhat310@users.noreply.github.com>
2023-03-15 23:34:00 +0530

    Update practical-04.java

--- java programs/practical-04.java
+++ java programs/practical-04.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class p_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Enter a number...");
         int num = sc.nextInt();
         int flag = 0;
